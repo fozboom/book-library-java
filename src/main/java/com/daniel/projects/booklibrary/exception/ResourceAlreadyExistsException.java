7e6a2432e70133d558ff0c9193de52dfa21c7f8c
fozboom <fozboom@gmail.com>
2024-04-02 10:44:50 +0300

    fix

--- src/main/java/com/daniel/projects/booklibrary/exception/ResourceAlreadyExistsException.java
+++ src/main/java/com/daniel/projects/booklibrary/exception/ResourceAlreadyExistsException.java
@@ -1,7 +1,5 @@
 package com.daniel.projects.booklibrary.exception;
 
-import java.io.Serial;
-
 public class ResourceAlreadyExistsException extends RuntimeException {
 	public ResourceAlreadyExistsException(final String message) {
 		super(message);
