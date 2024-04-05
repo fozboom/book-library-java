ec3935cffbbbbbdfccdb72fcf1e9cb2d5b2ecb24
fozboom <fozboom@gmail.com>
2024-04-05 10:22:17 +0300

    add tests to PublisherServiceTest

--- src/test/java/com/daniel/projects/booklibrary/service/PublisherServiceTest.java
+++ src/test/java/com/daniel/projects/booklibrary/service/PublisherServiceTest.java
@@ -3,7 +3,9 @@ package com.daniel.projects.booklibrary.service;
 import com.daniel.projects.booklibrary.dto.publisher.response.PublisherResponseDTO;
 import com.daniel.projects.booklibrary.dto.publisher.save.PublisherSaveDTO;
 import com.daniel.projects.booklibrary.exception.ResourceAlreadyExistsException;
+import com.daniel.projects.booklibrary.exception.ResourceNotFoundException;
 import com.daniel.projects.booklibrary.mapper.PublisherResponseDTOMapper;
+import com.daniel.projects.booklibrary.model.Book;
 import com.daniel.projects.booklibrary.model.Publisher;
 import com.daniel.projects.booklibrary.repository.BookRepository;
 import com.daniel.projects.booklibrary.repository.PublisherRepository;
@@ -95,6 +97,100 @@ class PublisherServiceTest {
 			publisherService.addPublisher(publisherSaveDTO);
 		});
 	}
+
+	@Test
+	void addPublishers_NotExistingNames() {
+		when(publisherRepository.existsByName(publisherSaveDTO.getName())).thenReturn(false);
+		when(publisherRepository.saveAll(anyList())).thenReturn(publishers);
+
+		assertEquals(publishers, publisherService.addPublishers(List.of(publisherSaveDTO)));
+	}
+
+	@Test
+	void addPublishers_ExistingNames() {
+		when(publisherRepository.existsByName(publisherSaveDTO.getName())).thenReturn(true);
+		assertEquals(new ArrayList<Publisher>(), publisherService.addPublishers(List.of(publisherSaveDTO)));
+	}
+
+	@Test
+	void testFindByName_Founded() {
+		when(publisherRepository.findOptionalByName(publisher.getName())).thenReturn(java.util.Optional.of(publisher));
+		when(publisherMapper.apply(publisher)).thenReturn(publisherResponseDTO);
+
+		assertEquals(publisherResponseDTO, publisherService.findByName(publisher.getName()));
+	}
+
+	@Test
+	void testFindByName_NotFounded() {
+		String name = publisher.getName();
+		when(publisherRepository.findOptionalByName(publisher.getName())).thenReturn(java.util.Optional.empty());
+		assertThrows(ResourceNotFoundException.class, () -> {
+			publisherService.findByName(name);
+		});
+	}
+
+	@Test
+	void testFindPublisherByIdFromCache() {
+		when(cacheService.getPublisher(publisher.getId())).thenReturn(publisher);
+		when(publisherMapper.apply(publisher)).thenReturn(publisherResponseDTO);
+
+		assertEquals(publisherResponseDTO, publisherService.findPublisherById(publisher.getId()));
+	}
+
+	@Test
+	void testFindPublisherByIdFromRepository() {
+		when(cacheService.getPublisher(publisher.getId())).thenReturn(null);
+		when(publisherRepository.findById(publisher.getId())).thenReturn(java.util.Optional.of(publisher));
+		when(publisherMapper.apply(publisher)).thenReturn(publisherResponseDTO);
+
+		assertEquals(publisherResponseDTO, publisherService.findPublisherById(publisher.getId()));
+	}
+
+	@Test
+	void testUpdatePublisherName_Founded() {
+		Long id = publisher.getId();
+		String newName = "New Publisher Name";
+		when(publisherRepository.findById(publisher.getId())).thenReturn(java.util.Optional.of(publisher));
+
+		assertDoesNotThrow(() -> {
+			publisherService.updatePublisherName(id, newName);
+		});
+	}
+
+	@Test
+	void testUpdatePublisherName_NotFounded() {
+		String newName = "New Publisher Name";
+		Long id = publisher.getId();
+		when(publisherRepository.findById(publisher.getId())).thenReturn(java.util.Optional.empty());
+
+		assertThrows(ResourceNotFoundException.class, () -> {
+			publisherService.updatePublisherName(id, newName);
+		});
+	}
+
+	@Test
+	void testDeletePublisherByName_Founded() {
+		Book book = new Book();
+		book.setPublisher(publisher);
+		book.setId(1L);
+		book.setTitle("Book title");
+
+		when(publisherRepository.findOptionalByName(publisher.getName())).thenReturn(java.util.Optional.of(publisher));
+		when(bookRepository.save(any(Book.class))).thenReturn(book);
+
+		publisher.getBooks().add(book);
+
+		assertDoesNotThrow(() -> {
+			publisherService.deletePublisherByName(publisher.getName());
+		});
+
+		verify(bookRepository).save(book);
+		verify(publisherRepository).delete(publisher);
+		verify(cacheService).removePublisher(publisher.getId());
+	}
+
+
+
 
 
 
